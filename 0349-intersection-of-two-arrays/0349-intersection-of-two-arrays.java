bb5fd50a44c0f65bde6ef426b7033b207496b5e9
vyshnavi-chippa <chippavyshnavi341@gmail.com>
2024-12-15 12:16:28 +0530

    Time: 21 ms (5.25%), Space: 43.7 MB (8.02%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,23 +1,26 @@
 class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> s1=new HashSet<>();
-        for(int i:nums1)
-        s1.add(i);
-        Set<Integer> s2=new HashSet<>();
-        for(int i:nums2)
-        s2.add(i);
-        List<Integer> l1=new ArrayList<>(s1);
-        List<Integer> l2=new ArrayList<>(s2);
-        List<Integer> l3=new ArrayList<>();
-        for(int i=0;i<l1.size();i++){
-            for(int j=0;j<l2.size();j++){
-                if(l1.get(i).equals(l2.get(j)))
-                l3.add(l1.get(i));
-            }
-        }
-        int a[]=new int[l3.size()];
-        for(int i=0;i<l3.size();i++)
-        a[i]=l3.get(i);
-        return a;
-    }
-}
+        public int[] intersection(int[] nums1, int[] nums2) {
+                Set<Integer> s1=new HashSet<>();
+                        for(int i:nums1)
+                                s1.add(i);
+                                        Set<Integer> s2=new HashSet<>();
+                                                for(int i:nums2)
+                                                        s2.add(i);
+                                                                List<Integer> l1=new ArrayList<>(s1);
+                                                                        List<Integer> l2=new ArrayList<>(s2);
+                                                                                List<Integer> l3=new ArrayList<>();
+                                                                                        for(int i=0;i<l1.size();i++){
+                                                                                                    for(int j=0;j<l2.size();j++){
+                                                                                                                    if(l1.get(i).equals(l2.get(j)))
+                                                                                                                                    l3.add(l1.get(i));
+                                                                                                                                                }
+                                                                                                                                                        }
+                                                                                                                                                                int a[]=new int[l3.size()];
+                                                                                                                                                                        for(int i=0;i<l3.size();i++)
+                                                                                                                                                                                a[i]=l3.get(i);
+                                                                                                                                                                                        return a;
+                                                                                                                                                                                            }
+                                                                                                                                                                                            }
+
+        
+    
