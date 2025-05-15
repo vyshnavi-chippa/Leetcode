c9f1ef79bd5d925aa0d2367575acc716b2fd4fe7
Vyshnavi Chippa <chippavyshnavi341@gmail.com>
2025-05-15 10:01:04 +0530

    Time: 0 ms (100%), Space: 42.6 MB (38.14%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int c=0;
+        for(int i:arr){
+            if(i%2!=0)
+            c++;
+            else
+            c=0;
+            if(c==3)
+            return true;
+        }
+        return false;
+    }
+}
