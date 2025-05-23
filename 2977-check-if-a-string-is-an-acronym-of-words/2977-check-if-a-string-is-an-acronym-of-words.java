fcc3b61f16fc39c0395c0ffa34f1d91e1794f456
Vyshnavi Chippa <chippavyshnavi341@gmail.com>
2025-05-24 00:11:49 +0530

    Time: 1 ms (100%), Space: 44.8 MB (50.91%) - LeetHub

---
+++ 2977-check-if-a-string-is-an-acronym-of-words/2977-check-if-a-string-is-an-acronym-of-words.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isAcronym(List<String> words, String s) {
+        if(s.length()!=words.size())
+        return false;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!=words.get(i).charAt(0))
+            return false;
+        }
+        return true;
+    }
+}
