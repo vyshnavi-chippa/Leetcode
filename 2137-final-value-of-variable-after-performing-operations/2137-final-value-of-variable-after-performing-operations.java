7a821ff82f9ae8e4592e55969f2f3f31669e6a65
Vyshnavi Chippa <chippavyshnavi341@gmail.com>
2025-05-30 21:54:32 +0530

    Time: 0 ms (100%), Space: 43 MB (23.01%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x=0;
+        for(int i=0;i<operations.length;i++){
+            if(operations[i].charAt(0)=='+'||operations[i].charAt(2)=='+')
+            x++;
+            else
+            x--;
+        }
+        return x;
+    }
+}
