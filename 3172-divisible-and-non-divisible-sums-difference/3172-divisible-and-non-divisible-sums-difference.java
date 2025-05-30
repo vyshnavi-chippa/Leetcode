1c57c1231bb625d1de161762898ba0fed30d3c53
Vyshnavi Chippa <chippavyshnavi341@gmail.com>
2025-05-30 21:54:51 +0530

    Time: 1 ms (61.77%), Space: 40.9 MB (54.2%) - LeetHub

---
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int s1=0,s2=0;
+        for(int i=1;i<=n;i++){
+            if(i%m==0)
+            s1+=i;
+            else
+            s2+=i;
+        }
+        return s2-s1;
+    }
+}
