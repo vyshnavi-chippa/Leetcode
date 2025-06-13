1d9204b1f013ab7d4887f3e9c0e152a9dbc284d9
Vyshnavi Chippa <chippavyshnavi341@gmail.com>
2025-06-13 23:05:51 +0530

    Time: 3 ms (41.29%), Space: 42.4 MB (6.48%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char ch1[]=s.toCharArray();
+        char ch2[]=t.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        for(int i=0;i<ch1.length;i++){
+            if(ch1[i]!=ch2[i])
+            return ch2[i];
+        }
+        return ch2[ch1.length];
+    }
+}
