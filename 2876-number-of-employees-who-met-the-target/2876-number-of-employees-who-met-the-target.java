36ef215afa547f88a7eea1fc7f6d91b492b3938d
Vyshnavi Chippa <chippavyshnavi341@gmail.com>
2025-07-02 16:58:51 +0530

    Time: 0 ms (100%), Space: 42.7 MB (24.06%) - LeetHub

---
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int c=0;
+        for(int i:hours){
+            if(i>=target)
+            c++;
+        }
+        return c;
+    }
+}
