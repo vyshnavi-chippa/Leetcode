883cabb6d656a96d79c97f5edf2d5ee0eef687c3
Vyshnavi Chippa <chippavyshnavi341@gmail.com>
2025-08-19 22:09:48 +0530

    Time: 3 ms (100%), Space: 76.8 MB (76.58%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,49 +1,18 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int p1=0,p2=0;
-        int n=nums.length;
-        int a[]=new int[n];
-        int idx=0;
-        while(p1<n&&p2<n){
-            if(idx%2==0){
-                if(nums[p1]>0){
-                a[idx]=nums[p1];
-                idx++;p1++;}
-                else{
-                    p1++;
-                }
+       int n=nums.length;
+       int a[]=new int[n];
+       int j=0,k=1,p=0;
+       for(int i=0;i<n;i++) {
+            if(nums[i]>0){
+                a[j]=nums[i];
+                j+=2;
             }
-            else if(idx%2!=0){
-                if(nums[p2]<0){
-                a[idx]=nums[p2];idx++;p2++;}
-                else{
-                    p2++;
-                }
+            if(nums[i]<0){
+                a[k]=nums[i];
+                k+=2;
             }
-            
-        }
-        while(p1<n){
-            
-                if(nums[p1]>0){
-                a[idx]=nums[p1];
-                idx++;p1++;}
-                else{
-                    p1++;
-                }
-            
-        }
-        while(p2<n){
-             
-                if(nums[p2]<0){
-                a[idx]=nums[p2];idx++;p2++;}
-                else{
-                    p2++;
-                }
-            
-        }
-        return a;
+       }
+       return a;
     }
 }
-//p1 0  1    2  3 4 5
-//p2 0  1 2  3      4 5 6
-//idx 0 1    2      3
